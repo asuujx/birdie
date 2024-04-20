bb5b8d57abc38640257d724708b45482c5da8888
assuy <43821373+assuy@users.noreply.github.com>
2024-04-20 15:03:50 +0200

    backend: Initial commit

---
+++ backend/src/test/java/com/birdie/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.birdie.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
