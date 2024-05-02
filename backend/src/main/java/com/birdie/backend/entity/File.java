153985d27bf414b9f681618b5c065af1bba0c02c
assuy <43821373+assuy@users.noreply.github.com>
2024-05-02 14:34:10 +0200

    backend: Add File entity

---
+++ backend/src/main/java/com/birdie/backend/entity/File.java
@@ -0,0 +1,29 @@
+package com.birdie.backend.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Getter
+@Setter
+
+@Entity
+@Table(name = "files")
+public class File {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "file_id", nullable = false)
+    private int id;
+
+    @Column(name = "file_path", nullable = false)
+    private String file_path;
+
+    @Column(name = "date_added", nullable = false)
+    private Date date_added;
+
+    @ManyToOne
+    @JoinColumn(name = "solution_id", nullable = false)
+    private Solution solution;
+}
