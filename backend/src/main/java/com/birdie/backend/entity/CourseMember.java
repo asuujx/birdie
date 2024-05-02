adae655985b4056cee7cbb9937dad4103f165615
assuy <43821373+assuy@users.noreply.github.com>
2024-05-02 14:33:48 +0200

    backend: Add CourseMember entity

---
+++ backend/src/main/java/com/birdie/backend/entity/CourseMember.java
@@ -0,0 +1,34 @@
+package com.birdie.backend.entity;
+
+import com.birdie.backend.entity.enummodels.UserType;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+
+@Entity
+@Table(name = "course_members")
+public class CourseMember {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "course_member_id", nullable = false)
+    private int id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "course_id", nullable = false)
+    private Course course;
+
+    @ManyToOne
+    @JoinColumn(name = "group_id")
+    private Group group;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "type", nullable = false)
+    private UserType type;
+}
