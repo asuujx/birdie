46dc81a37edb88b00f1a6f7f922fad959fcff74f
assuy <43821373+assuy@users.noreply.github.com>
2024-06-04 17:08:04 +0200

    backend: Take jwt logic back to the User controller

--- backend/src/main/java/com/birdie/backend/controllers/UserController.java
+++ backend/src/main/java/com/birdie/backend/controllers/UserController.java
@@ -2,36 +2,74 @@ package com.birdie.backend.controllers;
 
 import com.birdie.backend.dto.response.UserDetailsResponse;
 import com.birdie.backend.models.Course;
+import com.birdie.backend.models.CourseMember;
+import com.birdie.backend.models.User;
+import com.birdie.backend.repositories.CourseMemberRepository;
+import com.birdie.backend.services.JwtService;
 import com.birdie.backend.services.UserService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/account")
 public class UserController {
     private final UserService userService;
+    private final JwtService jwtService;
+    private final CourseMemberRepository courseMemberRepository;
 
     @Autowired
-    public UserController(UserService userService) {
+    public UserController(UserService userService, JwtService jwtService, CourseMemberRepository courseMemberRepository) {
         this.userService = userService;
+        this.jwtService = jwtService;
+        this.courseMemberRepository = courseMemberRepository;
     }
 
     @GetMapping
     @PreAuthorize("isAuthenticated()")
     public UserDetailsResponse getAccountDetails(@RequestHeader("Authorization") String token) {
-        return userService.getAccountDetails(token);
+        String jwt = token.replace("Bearer ", "");
+        UserDetails userDetails;
+
+        try {
+            userDetails = jwtService.loadUserDetailsFromToken(jwt);
+        } catch (Exception e) {
+            throw new RuntimeException("Invalid token", e);
+        }
+
+        if (jwtService.isTokenValid(jwt, userDetails)) {
+            User user = userService.getUserByEmail(userDetails.getUsername());
+            return new UserDetailsResponse(user.getId(), user.getEmail(), user.getName(), user.getSurname(), user.getRole());
+        } else {
+            throw new RuntimeException("Invalid or expired token");
+        }
     }
 
     @GetMapping("/courses")
     @PreAuthorize("isAuthenticated()")
     public List<Course> getAccountCourses(@RequestHeader("Authorization") String token) {
-        return userService.getAccountCourses(token);
+        String jwt = token.replace("Bearer ", "");
+        UserDetails userDetails;
+
+        try {
+            userDetails = jwtService.loadUserDetailsFromToken(jwt);
+        } catch (Exception e) {
+            throw new RuntimeException("Invalid token", e);
+        }
+
+        User user = userService.getUserByEmail(userDetails.getUsername());
+
+        return courseMemberRepository.findByUser(user)
+                .stream()
+                .map(CourseMember::getCourse)
+                .collect(Collectors.toList());
     }
 }
--- backend/src/main/java/com/birdie/backend/services/UserService.java
+++ backend/src/main/java/com/birdie/backend/services/UserService.java
@@ -1,6 +1,5 @@
 package com.birdie.backend.services;
 
-import com.birdie.backend.dto.response.UserDetailsResponse;
 import com.birdie.backend.models.Course;
 import com.birdie.backend.models.CourseMember;
 import com.birdie.backend.models.User;
@@ -19,12 +18,10 @@ import java.util.stream.Collectors;
 public class UserService implements UserDetailsService {
     private final UserRepository userRepository;
     private final CourseMemberRepository courseMemberRepository;
-    private final JwtService jwtService;
 
-    public UserService(UserRepository userRepository, CourseMemberRepository courseMemberRepository, JwtService jwtService) {
+    public UserService(UserRepository userRepository, CourseMemberRepository courseMemberRepository) {
         this.userRepository = userRepository;
         this.courseMemberRepository = courseMemberRepository;
-        this.jwtService = jwtService;
     }
 
     @Override
@@ -40,42 +37,6 @@ public class UserService implements UserDetailsService {
 
     public UserDetailsService userDetailsService() {
         return username -> userRepository.findByEmail(username).orElseThrow(() -> new UsernameNotFoundException("Username not found."));
-    }
-
-    public UserDetailsResponse getAccountDetails(String token) {
-        String jwt = token.replace("Bearer ", "");
-        UserDetails userDetails;
-
-        try {
-            userDetails = jwtService.loadUserDetailsFromToken(jwt);
-        } catch (Exception e) {
-            throw new RuntimeException("Invalid token", e);
-        }
-
-        if (jwtService.isTokenValid(jwt, userDetails)) {
-            User user = getUserByEmail(userDetails.getUsername());
-            return new UserDetailsResponse(user.getId(), user.getEmail(), user.getName(), user.getSurname(), user.getRole());
-        } else {
-            throw new RuntimeException("Invalid or expired token");
-        }
-    }
-
-    public List<Course> getAccountCourses(String token) {
-        String jwt = token.replace("Bearer ", "");
-        UserDetails userDetails;
-
-        try {
-            userDetails = jwtService.loadUserDetailsFromToken(jwt);
-        } catch (Exception e) {
-            throw new RuntimeException("Invalid token", e);
-        }
-
-        User user = getUserByEmail(userDetails.getUsername());
-
-        return courseMemberRepository.findByUser(user)
-                .stream()
-                .map(CourseMember::getCourse)
-                .collect(Collectors.toList());
     }
 
     public User save(User newUser) {
