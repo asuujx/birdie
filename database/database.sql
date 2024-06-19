824b71900001155d2d944188c4f97be1d168db67
asuujx <43821373+asuujx@users.noreply.github.com>
2024-06-19 11:39:15 +0200

    database: Change files table

--- database/database.sql
+++ database/database.sql
@@ -2,8 +2,8 @@
 -- PostgreSQL database dump
 --
 
--- Dumped from database version 16.2
--- Dumped by pg_dump version 16.2
+-- Dumped from database version 16.3
+-- Dumped by pg_dump version 16.3
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -123,9 +123,10 @@ ALTER SEQUENCE public.courses_id_seq OWNED BY public.courses.course_id;
 
 CREATE TABLE public.files (
     file_id integer NOT NULL,
-    file_path character varying(255) NOT NULL,
+    url character varying(255) NOT NULL,
     date_added timestamp without time zone NOT NULL,
-    solution_id integer NOT NULL
+    solution_id integer NOT NULL,
+    original_name character varying(255)
 );
 
 
@@ -356,6 +357,7 @@ ALTER TABLE ONLY public.users ALTER COLUMN user_id SET DEFAULT nextval('public.u
 COPY public.course_members (course_member_id, user_id, course_id, group_id, status) FROM stdin;
 1	9	1	\N	PENDING
 2	7	4	\N	ACTIVE
+3	9	2	\N	PENDING
 \.
 
 
@@ -375,7 +377,25 @@ COPY public.courses (course_id, name) FROM stdin;
 -- Data for Name: files; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-COPY public.files (file_id, file_path, date_added, solution_id) FROM stdin;
+COPY public.files (file_id, url, date_added, solution_id, original_name) FROM stdin;
+4	http://localhost:8080/f20e52c1-1a0e-4fc0-a0db-e9eba15dd520-3aaf9148a901c547e1306a00b3d9164b.jpg	2024-06-14 14:08:53.722	6	3aaf9148a901c547e1306a00b3d9164b.jpg
+5	http://localhost:8080/5c1f7d6c-e162-4def-9761-bb3f7eb617d1-502d1bc3133159b48d5df895c5eb83eb.jpg	2024-06-14 14:08:53.722	6	502d1bc3133159b48d5df895c5eb83eb.jpg
+6	http://localhost:8080/a7c76e7d-5eef-45e9-8f5f-8ab61d8ef96e-559f276d64e08ed43cfa78d418853882.jpg	2024-06-14 14:08:53.722	6	559f276d64e08ed43cfa78d418853882.jpg
+7	http://localhost:8080/eba9f2ff-3458-48e1-96cd-aed26d7523cc-3aaf9148a901c547e1306a00b3d9164b.jpg	2024-06-14 14:09:24.796	7	3aaf9148a901c547e1306a00b3d9164b.jpg
+8	http://localhost:8080/419b86b3-24a3-4e62-bd14-7ee665fb7480-502d1bc3133159b48d5df895c5eb83eb.jpg	2024-06-14 14:09:24.796	7	502d1bc3133159b48d5df895c5eb83eb.jpg
+9	http://localhost:8080/ae9ca725-3e1a-46c0-89dd-8dbcfc3563f2-559f276d64e08ed43cfa78d418853882.jpg	2024-06-14 14:09:24.796	7	559f276d64e08ed43cfa78d418853882.jpg
+10	http://localhost:8080/96ccb73f-3fe8-4a42-bc12-2adbfbb0bd9e-3aaf9148a901c547e1306a00b3d9164b.jpg	2024-06-14 15:21:16.427	8	3aaf9148a901c547e1306a00b3d9164b.jpg
+11	http://localhost:8080/e75c5628-c264-42ab-8ace-5eae8b053e59-502d1bc3133159b48d5df895c5eb83eb.jpg	2024-06-14 15:21:16.427	8	502d1bc3133159b48d5df895c5eb83eb.jpg
+12	http://localhost:8080/025d9a7f-fd87-4137-b483-df20e42f1c60-559f276d64e08ed43cfa78d418853882.jpg	2024-06-14 15:21:16.427	8	559f276d64e08ed43cfa78d418853882.jpg
+13	http://localhost:8080/ed63ec76-f671-4936-a148-579511e7729f-3aaf9148a901c547e1306a00b3d9164b.jpg	2024-06-14 15:34:36.499	9	3aaf9148a901c547e1306a00b3d9164b.jpg
+14	http://localhost:8080/ad1b81b3-6a3e-44ae-989c-eb3f0f135b89-502d1bc3133159b48d5df895c5eb83eb.jpg	2024-06-14 15:34:36.499	9	502d1bc3133159b48d5df895c5eb83eb.jpg
+15	http://localhost:8080/1a3aeb26-8762-479f-9c31-2df150fe2340-559f276d64e08ed43cfa78d418853882.jpg	2024-06-14 15:34:36.499	9	559f276d64e08ed43cfa78d418853882.jpg
+16	http://localhost:8080/449d5653-1f11-47d5-85e9-635085fe77a6-3aaf9148a901c547e1306a00b3d9164b.jpg	2024-06-14 15:35:09.844	10	3aaf9148a901c547e1306a00b3d9164b.jpg
+17	http://localhost:8080/cccf8af8-f2ae-42fb-9ef3-e5d916b2d3ff-502d1bc3133159b48d5df895c5eb83eb.jpg	2024-06-14 15:35:09.844	10	502d1bc3133159b48d5df895c5eb83eb.jpg
+18	http://localhost:8080/32ffb99e-835f-4e07-9f9c-dd5445961a6b-559f276d64e08ed43cfa78d418853882.jpg	2024-06-14 15:35:09.844	10	559f276d64e08ed43cfa78d418853882.jpg
+24	http://localhost:8080/2e5354a5-2e76-4450-9bad-536392605ea4-502d1bc3133159b48d5df895c5eb83eb - Copy.jpg	2024-06-14 15:39:23.585	12	502d1bc3133159b48d5df895c5eb83eb - Copy.jpg
+25	http://localhost:8080/cdb811c4-17d5-4fdb-913e-1ea26da70d40-502d1bc3133159b48d5df895c5eb83eb.jpg	2024-06-14 15:39:23.585	12	502d1bc3133159b48d5df895c5eb83eb.jpg
+26	http://localhost:8080/6ae7bf33-9f8c-4478-9409-cb02c19917dd-559f276d64e08ed43cfa78d418853882.jpg	2024-06-14 15:39:23.585	12	559f276d64e08ed43cfa78d418853882.jpg
 \.
 
 
@@ -392,6 +412,12 @@ COPY public.groups (group_id, course_id, name) FROM stdin;
 --
 
 COPY public.solutions (solution_id, course_member_id, task_id, date_added) FROM stdin;
+6	3	1	2024-06-14 14:08:53.722
+7	3	1	2024-06-14 14:09:24.796
+8	3	1	2024-06-14 15:21:16.427
+9	3	1	2024-06-14 15:34:36.499
+10	3	1	2024-06-14 15:35:09.844
+12	3	1	2024-06-14 15:39:23.585
 \.
 
 
@@ -400,6 +426,7 @@ COPY public.solutions (solution_id, course_member_id, task_id, date_added) FROM 
 --
 
 COPY public.tasks (task_id, course_id, name, description, due_date) FROM stdin;
+1	2	dupa		2024-06-09 17:19:11.948
 \.
 
 
@@ -419,7 +446,7 @@ COPY public.users (user_id, email, password, name, surname, role, avatar) FROM s
 -- Name: course_members_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('public.course_members_id_seq', 2, true);
+SELECT pg_catalog.setval('public.course_members_id_seq', 3, true);
 
 
 --
@@ -433,7 +460,7 @@ SELECT pg_catalog.setval('public.courses_id_seq', 4, true);
 -- Name: files_file_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('public.files_file_id_seq', 1, false);
+SELECT pg_catalog.setval('public.files_file_id_seq', 26, true);
 
 
 --
@@ -447,14 +474,14 @@ SELECT pg_catalog.setval('public.groups_group_id_seq', 1, false);
 -- Name: solutions_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('public.solutions_id_seq', 1, false);
+SELECT pg_catalog.setval('public.solutions_id_seq', 12, true);
 
 
 --
 -- Name: tasks_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('public.tasks_id_seq', 1, false);
+SELECT pg_catalog.setval('public.tasks_id_seq', 1, true);
 
 
 --
@@ -485,7 +512,7 @@ ALTER TABLE ONLY public.courses
 --
 
 ALTER TABLE ONLY public.files
-    ADD CONSTRAINT files_file_path_key UNIQUE (file_path);
+    ADD CONSTRAINT files_file_path_key UNIQUE (url);
 
 
 --
